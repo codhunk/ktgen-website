a0dc562f2ae04dee453b0a5888f5293740a44965
rtk-group <mydevclass9690@gmail.com>
2025-07-16 18:53:19 +0530

    complete pregnency section

--- src/pages/Pregnency_sec.jsx
+++ src/pages/Pregnency_sec.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { useDarkMode } from '../hooks/UseDarkMode/useDarkMode';
 import { useTranslation } from "react-i18next";
+
 
 const videoData = [
   { label: 'Week 1', url: '/videos/week1.mp4' },
@@ -17,6 +19,8 @@ const videoData = [
 ];
 
 const Pregnency_sec = () => {
+  const { isDark, toggleDarkMode } = useDarkMode();
+
   const { t } = useTranslation();
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -27,32 +31,30 @@ const Pregnency_sec = () => {
   };
 
   return (
+    <main className='dark:bg-[#4B5945] dark:text-white'>
     <div className="p-6 max-w-5xl mx-auto space-y-10">
       {/* Banner */}
-      <div style={{backgroundImage:"url('/baby.jpg')", backgroundSize: 'cover'}} className="bg-cover bg-center bg-gray-200 p-8 md:p-20 rounded-xl text-center">
-        <h1 className="text-4xl font-bold text-gray-700">{t("pregnency_j")}</h1>
-        <p className="mt-2 text-gray-700 text-lg">{t("preg_explore")}</p>
+      <div style={{backgroundImage:"url('/baby.jpg')"}} className="bg-cover text-gray-800 dark:text-[#4B5945] bg-center bg-gray-200 p-8 md:p-20 rounded-xl text-center">
+        <h1 className="text-4xl font-bold">{t("pregnency_j")}</h1>
+        <p className="mt-2 text-lg">{t("preg_explore")}</p>
       </div>
 
       {/* About */}
-      <div className="bg-white p-6 rounded-xl shadow">
+      <div className="bg-white dark:bg-[#66785F] dark:text-white text-gray-700  p-6 rounded-xl shadow">
         <h2 className="text-2xl font-semibold mb-3">{t("preg_about")}</h2>
-        <p className="text-gray-700">{t("preg_h")}</p>
+        <p className="">{t("preg_h")}</p>
       </div>
 
       {/* Video Playlist */}
-      <div className="bg-gray-100 p-6 rounded-xl shadow">
-        <h3 className="text-xl font-semibold mb-4">{t("preg_watch")}</h3>
+      <div className="bg-gray-100 dark:bg-[#66785F] p-6 rounded-xl shadow">
+        <h3 className="text-xl font-semibold mb-4 text-gray-700 dark:text-white ">{t("preg_watch")}</h3>
         <div className="flex flex-wrap gap-3 mb-4">
           {videoData.map((video, idx) => (
             <button
               key={idx}
               onClick={() => setCurrentIndex(idx)}
-              className={`px-3 py-1 rounded-full text-sm border transition ${
-                currentIndex === idx
-                  ? 'bg-gray-600 text-white'
-                  : 'bg-white hover:bg-gray-100 text-gray-700'
-              }`}
+              className={`px-3 py-1 rounded-full text-sm border ${
+                currentIndex === idx ? 'bg-gray-700 dark:bg-[#4B5945] text-white':'bg-white dark:bg-[#B2C9AD] dark:hover:bg-[#66785F] hover:bg-gray-100 text-[#4B5945]'}`}
             >
               {video.label}
             </button>
@@ -71,6 +73,7 @@ const Pregnency_sec = () => {
         </video>
       </div>
     </div>
+    </main>
   );
 };
 
