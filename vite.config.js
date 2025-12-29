2325c84819c074b1c5bd7be84c9c16e93eda25f5
codhunk <jalandhar143184@gmail.com>
2025-12-29 17:24:38 +0530

    run

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 1000 // 1 MB
-  },
+
   plugins: [react()],
   server: {
     host: "0.0.0.0", // Allow external access
